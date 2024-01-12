b7d04813ca0d3f78e1c95cbc8462f16db9d61264
Mannyking <68853328+Mannyking@users.noreply.github.com>
2024-01-12 23:00:57 +0100

    Updates 2 - Jan 12, 2024

--- src/main/java/code/solutions/numbers/NumberCalculations.java
+++ src/main/java/code/solutions/numbers/NumberCalculations.java
@@ -1,4 +1,51 @@
 package code.solutions.numbers;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberCalculations {
+    public static void main(String[] args) {
+        int[] a = {};
+        int[] b = {};
+        System.out.println(comp(a, b));
+    }
+
+// Given an array of integers, find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+    public static int findIt(int[] a) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int number : a) {
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        }
+        for (Integer result : map.keySet()) {
+            if (map.get(result) % 2 != 0) {
+                return result;
+            }
+        }
+        return 0;
+    }
+
+//  function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears).
+//  "Same" means, here, that the elements in b are the elements in a squared, regardless of the order
+    public static boolean comp(int[] a, int[] b) {
+        if (a == null || b == null || a.length != b.length) return false;
+        if (a.length == 0) return true;
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] * a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+//function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer
+    public int cockroachSpeed(double x) {
+        double kmToCm = x * 100000;
+        double hourToSeconds = 60 * 60;
+        return (int) (kmToCm / hourToSeconds);
+    }
 }
--- src/main/java/code/solutions/strings/StringManipulation.java
+++ src/main/java/code/solutions/strings/StringManipulation.java
@@ -1,3 +1,46 @@
 package code.solutions.strings;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringManipulation {
+    public static void main(String[] args) {
+        System.out.println(solution("world"));
+    }
+// reversed strings
+    public static String solution(String str) {
+        StringBuilder stringBuilder = new StringBuilder(str);
+        stringBuilder.reverse();
+        return stringBuilder.toString();
+    }
+
+// number (count) of vowels in the given string
+    public static int getCount(String str) {
+        Pattern pattern = Pattern.compile("aeiou");
+        Matcher matcher = pattern.matcher(str);
+        int count = 0;
+        while (matcher.find()) count++;
+        return count;
+    }
+
+// Remove all the vowels in a string
+    public static String removeVowels(String str) {
+        String result;
+        result = str.replaceAll("[aeiouAEIOU]", "");
+        return result;
+    }
+
+// function printer_error which given a string will return the error rate of the printer as a string representing a rational
+// whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression
+    public static String printerError(String s) {
+        Pattern pattern = Pattern.compile("(?i)[n-z]");
+        Matcher matcher = pattern.matcher(s);
+        int start = 0;
+
+        while (matcher.find()) {
+            start++;
+        }
+        return start + "/" + s.length();
+    }
 }
